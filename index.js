7b2e6e0bd46c6156460035d79b828f3a668c493f
Michal Budzynski <michal@virtualdesign.pl>
2014-07-18 18:14:48 +0200

    [MULTIPLE-EQUAL] - :bento: lint fixes

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-var deepEqual = function( x, y ) {
-  if ( x === y ) return true;
-  if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
-  if ( x.constructor !== y.constructor ) return false;
-  for ( var p in x ) {
-    if ( ! x.hasOwnProperty( p ) ) continue;
-    if ( ! y.hasOwnProperty( p ) ) return false
-    if ( x[ p ] === y[ p ] ) continue;
-    if ( typeof( x[ p ] ) !== "object" ) return false;
-    if ( ! Object.equals( x[ p ],  y[ p ] ) ) return false;
+'use strict';
+
+var deepEqual = function(x, y) {
+  if (x === y) {
+    return true;
   }
-  for ( p in y ) {
-    if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+  if (!(x instanceof Object) || ! (y instanceof Object)) {
+    return false;
+  }
+  if (x.constructor !== y.constructor) {
+    return false;
+  }
+  for (var p in x) {
+
+    if (! x.hasOwnProperty(p)) {
+      continue;
+    }
+
+    if (! y.hasOwnProperty(p)) {
+      return false;
+    }
+
+    if (x[p] === y[p]) {
+      continue;
+    }
+
+    if (typeof(x[p]) !== 'object') {
+      return false;
+    }
+
+    if (! Object.equals(x[p],  y[p])) {
+      return false;
+    }
+  }
+  for (p in y) {
+    if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) {
+      return false;
+    }
   }
   return true;
-}
+};
 
 var multipleEqual = function(){
   var args = Array.prototype.slice.call(arguments);
   return args.slice(1).every(
     function(element) {
-      return deepEqual(args[0], element)
+      return deepEqual(args[0], element);
     }
-  );
-}
+ );
+};
 
 module.exports = multipleEqual;
